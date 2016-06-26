b62b2142dcb4064e70e598b7fb1bf5013b5ec082
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2016-06-26 16:41:34 -0700

    Transmit barometric pressure, break out flashy bits.

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -3,6 +3,7 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
 #include "DHT.h"
+#include <string.h>
 
 #define DHTPIN 2     // what digital pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
@@ -20,6 +21,29 @@ RFTransmitter transmitter(TRANSMITTER_PIN, NODE_ID);
 // Thanks, AdaFruit!
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
 
+char *hlegend = "H: ";
+char *tlegend = "T: ";
+char *plegend = "P: ";
+
+float h;
+float t;
+
+char hmsg[6];
+char tmsg[6];
+char pmsg[8];
+sensors_event_t event;
+
+void flashyflashy() {
+        digitalWrite(LEDPIN, HIGH);
+        delay(125);
+        digitalWrite(LEDPIN, LOW);
+        delay(125);
+        digitalWrite(LEDPIN, HIGH);
+        delay(125);
+        digitalWrite(LEDPIN, LOW);
+        delay(125);
+}
+
 void setup() {
         Serial.begin(9600);
         pinMode(LEDPIN, OUTPUT);
@@ -32,31 +56,57 @@ void setup() {
                 Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
                 while(1);
         }
-
 }
 
 void loop() {
         digitalWrite(LEDPIN, HIGH);
         delay(500);
         digitalWrite(LEDPIN, LOW);
-        float h = dht.readHumidity();
+        h = dht.readHumidity();
+        // 12.45\0
+        hmsg[6];
+        // args: (src, width, precision, dest)
+        dtostrf(h, 4, 2, hmsg);
         // Read temperature as Celsius (the default)
-        float t = dht.readTemperature();
+        t = dht.readTemperature();
+        // 12.45\0
+        tmsg[6];
+        // args: (src, width, precision, dest)
+        dtostrf(t, 4, 2, tmsg);
         /* Get a new sensor event */
-        sensors_event_t event;
+        //
         bmp.getEvent(&event);
         if (event.pressure) {
                 /* Display atmospheric pressure in hPa */
-                Serial.print("Pressure: "); Serial.print(event.pressure); Serial.println(" hPa");
+                Serial.print("Pressure: ");
+                Serial.print(event.pressure);
+                Serial.println(" hPa");
         }
         else {
                 Serial.println("Sensor error");
         }
 
-        char *msg = "Hello World!";
-        char hmsg[10];
-        // args: (src, width, precision, dest)
-        dtostrf(h, 4, 2, hmsg);
+        // 1234.67\0
+        dtostrf(event.pressure, 6, 2, pmsg);
+        // now combine.  See https://www.reddit.com/r/arduino/comments/2zzd41/need_help_combining_2_char_arrays/
+        char final_msg[sizeof(tlegend) +
+                       sizeof(tmsg) +
+                       1 +      // " "
+                       sizeof(hlegend) +
+                       sizeof(hmsg) +
+                       1 +      // " "
+                       sizeof(plegend) +
+                       sizeof(pmsg)
+                       ];
+        strcat(final_msg, tlegend);
+        strcat(final_msg, tmsg);
+        strcat(final_msg, " ");
+        strcat(final_msg, hlegend);
+        strcat(final_msg, hmsg);
+        strcat(final_msg, " ");
+        strcat(final_msg, plegend);
+        strcat(final_msg, pmsg);
+        // char *msg = "Hello World!";
         //char *msg = malloc(strlen(printf("Hello, world! T: %f H: %f", t, h)));
         // char *msg;
         // sprintf(msg, "Hello, world!  T: %f H: %f", t, h);
@@ -64,21 +114,15 @@ void loop() {
         Serial.println("I'm here...");
         Serial.print("Humidity: ");
         Serial.print(h);
-        Serial.print("Temp: ");
+        Serial.print(" Temp: ");
         Serial.print(t);
         Serial.println();
+        Serial.println(final_msg);
         // transmitter.send((byte *)msg, strlen(msg) + 1);
-        transmitter.send((byte *)hmsg, strlen(hmsg) + 1);
+        transmitter.send((byte *)final_msg, strlen(final_msg) + 1);
         //transmitter.send(textmsg, textmsg.length());
-        digitalWrite(LEDPIN, HIGH);
-        delay(125);
-        digitalWrite(LEDPIN, LOW);
-        delay(125);
-        digitalWrite(LEDPIN, HIGH);
-        delay(125);
-        digitalWrite(LEDPIN, LOW);
-        delay(125);
+        flashyflashy();
         delay(1000);
         // transmitter.resend((byte *)msg, strlen(msg) + 1);
-        transmitter.resend((byte *)hmsg, strlen(hmsg) + 1);
+        transmitter.resend((byte *)final_msg, strlen(final_msg) + 1);
 }
