a8c0a730e08e8dadd3bee638e2dd89d06f6b5be1
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2016-08-14 19:16:41 -0700

    More detail.

---
+++ logger/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+INFLUX_URL=${INFLUX_URL:https://saintaardvarkthecarpeted.com:26472}
+INFLUX_DB=${INFLUX_DB:weather}
+
+if [[ -z $INFLUX_USER ]] ; then
+    echo "INFLUX_USER unset!"
+    exit 1
+elif [[ -z $INFLUX_PASS ]] ; then
+    echo "INFLUX_PASS unset!"
+    exit 1
+fi
+
+docker run \
+       -e INFLUX_USER=${INFLUX_USER} \
+       -e INFLUX_PASS=${INFLUX_PASS} \
+       -e INFLUX_URL=\"${INFLUX_URL}\" \
+       -e INFLUX_DB=${INFLUX_DB} \
+       --device=/dev/ttyACM0 \
+       arduino_wx
