17e3fd4071acf60acf308074821c59c1f3976601
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2017-03-29 15:51:29 -0700

    Indentation fix

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -8,18 +8,19 @@ const char endOfMessage = '|';
 int index;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Waiting...");
-  receiver.begin();
+        Serial.begin(9600);
+        Serial.println("Waiting...");
+        receiver.begin();
 }
 
 void loop() {
-  char msg[MAX_PACKAGE_SIZE];
-  byte senderId = 0;
-  byte packageId = 0;
-  byte len = receiver.recvPackage((byte *)msg, &senderId, &packageId);
-  payload = String(msg);
-  index = payload.indexOf(endOfMessage);
-  payload.remove(index);
-  Serial.println(payload);
+        Serial.println("FIXME: Made it to beginning of loop");
+        char msg[MAX_PACKAGE_SIZE];
+        byte senderId = 0;
+        byte packageId = 0;
+        byte len = receiver.recvPackage((byte *)msg, &senderId, &packageId);
+        payload = String(msg);
+        /* index = payload.indexOf(endOfMessage); */
+        /* payload.remove(index); */
+        Serial.println(payload);
 }
